f24c040e9f8730a953a81603f48abd13300e9914
kwakkyeongrok <kyeongrok@naver.com>
2023-10-07 19:56:08 +0900

    modify url name

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,44 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { Col, Container, Row, Dropdown, Button } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row, Dropdown, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   const [product, setProduct] = useState(null);
   let { id } = useParams();
   const getProductDetail = async () => {
-    let url = `http://localhost:3004/products/${id}`;
+    let url = `https://my-json-server.typicode.com/rok92/hnm-shoppingmall/products/${id}`;
     let res = await fetch(url);
     let data = await res.json();
     setProduct(data);
-  }
+  };
 
   useEffect(() => {
     getProductDetail();
-  }, [])
+  }, []);
   return (
     <Container>
       <Row>
-        <Col className='img_container'>
+        <Col className="img_container">
           <img src={product?.img} />
         </Col>
         <Col>
-          <div className='product_letter'>{product?.title}</div>
-          <div className='product_letter'>₩{product?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</div>
-          <div className='dropdown_p'>{product?.choice ? "Conciuos Choice" : ""}</div>
+          <div className="product_letter">{product?.title}</div>
+          <div className="product_letter">
+            ₩{product?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+          </div>
+          <div className="dropdown_p">
+            {product?.choice ? "Conciuos Choice" : ""}
+          </div>
           <Dropdown>
             <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
               Select Size
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-              {product?.size.length > 0 && product.size.map((item) => (<Dropdown.Item href="#/action-1">{item}</Dropdown.Item>))}
+              {product?.size.length > 0 &&
+                product.size.map((item) => (
+                  <Dropdown.Item href="#/action-1">{item}</Dropdown.Item>
+                ))}
             </Dropdown.Menu>
           </Dropdown>
           <Button variant="danger">Add</Button>
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
